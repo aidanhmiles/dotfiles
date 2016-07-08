4b6809e1619fdc1288989416cc1983cdf8a22620
Aidan Miles <amiles@seamgen.com>
2016-07-08 11:17:41 -0700

    adds rgx.sh, a file which makes figuring out bash regexps more convenient

---
+++ rgx.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: $0 <REGEXP PATTERN STRING> <TEST STRINGS>"
+    exit 1
+fi
+
+regex=$1
+shift
+echo "regex: $regex"
+echo
+
+while [[ $1 ]]
+do
+    if [[ $1 =~ $regex ]]; then
+        echo "$1 matches: ${BASH_REMATCH[0]}"
+        i=1
+        n=${#BASH_REMATCH[*]}
+
+		if [[ $n -le 1 ]]; then
+			echo "...but no capture groups"
+		fi
+
+        while [[ $i -lt $n ]]; do
+			echo
+            echo "  capture[$i]: ${BASH_REMATCH[$i]}"
+            let i++
+        done
+    else
+		echo
+        echo "$1 does not match"
+    fi
+    shift
+done
