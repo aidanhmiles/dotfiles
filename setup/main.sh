5edf8b3acffd761e5594e49d97e029c85736880f
Aidan Miles <aidanhmiles@gmail.com>
2020-03-22 12:18:41 -0700

    Adds the latest of the setup/ dir

--- setup/main.sh
+++ setup/main.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Run this script using with:
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/aidanhmiles/dotfiles/master/setup/main.sh)"
+
 # ansible pre-reqs:
 #   xcode CLI utils
 #   homebrew
@@ -7,58 +10,77 @@
 #   ansible
 
 # Step 0: Idiot-check
-# stays false in source control so it can't be run immediately after cloning 
-readonly read_the_instructions=false
-[[ $checked != true ]] && echo "Read the instructions first!" && exit 1;
+read -p "Have you read the instructions? (only 'yes' is accepted)" idiotcheck
+case $idiotcheck in
+  yes ) pass=true; break;;
+  * ) pass=false; break;;
+esac
+
+[[ $pass != true ]] && echo "Read the instructions first!" && exit 1;
 
 # Step 1: XCODE 
+echo "checking xcode (xcode-select -p)"
 xcode-select -p
 xcode_status=$? 
 if [[ $xcode_status -eq 0 ]]; then
   echo "XCode is fine"
 else
-  echo "running `xcode-select --install`"
+  echo "Need to install xcode CLI tools"
+  echo "xcode-select --install"
   xcode-select --install
 fi
 
-# Step 2: Homebrew
+# Now we for sure have Git
+# TODO how to make this idempotent
+# Step 2: Clone dotfiles
+git clone https://github.com/aidanhmiles/dotfiles.git ~/dotfiles
+
+cd ~/dotfiles
+
+# Step 3: Homebrew
 which brew 
 brew_status=$?
 if [[ ! $brew_status -eq 0 ]]; then
   echo ""
   echo "Installing Homebrew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
   echo ""
   echo "Updating Homebrew (brew update)"
   brew update
-  echo "Installing everything in Brewfile"
+  echo "Installing everything in Brewfile (brew bundle)"
   brew bundle --file="~/dotfiles/Brewfile"
 fi
 
-# We now have *env, updated bash, our favorite shell utils, and more
-# Step 3: Python
-pyenv install 3.8.1
-pyenv global 3.8.1
-pip install --upgrade pip
-pip install ansible
+# We now have pyenv, updated bash, our favorite shell utils, and more
+# TODO do we need to reload the shell here?
+# source ~/.bashrc  
+# etc
 
-# Install ansible
+# Step 4: Python
+echo "pyenv install 3.8.1"
+pyenv install 3.8.1
+echo "pyenv global 3.8.1"
+pyenv global 3.8.1
+
+pip install --upgrade pip
+echo "pip install --upgrade pip"
+
+# Step 5: Install ansible
 which ansible
 ansible_status=$?
 if [[ ! $ansible_status -eq 0 ]]; then
   echo ""
-  echo "Installing Ansible"
+  echo "pip install ansible"
   pip install ansible
-  
 fi
 
-ansible macos_playbook.yml
+# Step 6: Run ansible
+echo "ansible macos_playbook.yml"
 
 # the command
 ANSIBLE_CFG="./ansible.cfg" \
-AWS_PROFILE=$AWS_PROFILE \
 ansible-playbook \
 playbooks/macos_playbook.yml
 
-
+# maybe some more here
