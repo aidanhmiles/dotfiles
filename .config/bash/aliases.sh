28c299c82c28eadc662b214e8f9886ea8805f505
Aidan Miles <aidanhmiles@gmail.com>
2020-03-26 11:12:53 -0700

    adds a new alias for a notes dir

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -13,6 +13,8 @@ alias sshconf="vim ~/.ssh/config"
 # "scratch paper"
 alias scratch="vim ~/Desktop/scratch"
 
+alias notes="vp ~/Documents/avr/stickynotes/*"
+
 # changing bash commands
 # for highlighting different entities when ls-ing around
 LS_COLORS='di=36;40:ln=35;40:so=32;40:pi=32;40:ex=31;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
